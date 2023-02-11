12ae619ff35fc91e345b4a52d76c4b986b55eaf5
alemtani <alex.m.tanimoto@gmail.com>
2023-02-11 15:55:17 -0500

    Use env vars for db conn

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,10 @@
 const Pool = require('pg').Pool;
 
+require('dotenv').config();
+
 const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
     host: 'localhost',
     port: 5432,
     database: 'ccsb'
