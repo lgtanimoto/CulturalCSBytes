26d4ae7f96e258ba18191726cc881438ba65da39
alemtani <alex.m.tanimoto@gmail.com>
2023-03-21 21:32:39 -0700

    Add default conn opts for db

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,8 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DBUSER,
-    password: process.env.DBPW,
+    user: process.env.DBUSER || 'postgres',
+    password: process.env.DBPW || 'postgres',
     host: process.env.DBHOST || 'localhost',
     port: process.env.DBPORT || 5432,
     database: process.env.DBNAME || 'ccsb'
